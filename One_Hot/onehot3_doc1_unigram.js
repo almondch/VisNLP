e8a6d80675043a1ecccd709bed91217147971d15
almondch <47764404+almondch@users.noreply.github.com>
2021-07-29 16:16:33 +0700

    Update onehot3_doc1_unigram.js

--- One_Hot/onehot3_doc1_unigram.js
+++ One_Hot/onehot3_doc1_unigram.js
@@ -20,7 +20,7 @@ d3.json("One_Hot/ohe_step2_doc1_unigram.json", function(data) {
 		.domain([0,1])
 		.range(["white", "#b30000"]);
 	
-	var svg = d3.select("#ohe_step3").append("svg")
+	var svg = d3.select("ohe_step3").append("svg")
 		.attr("viewBox", "0 0 1600 1200")
 		.attr("preserveAspectRatio", "xMinYMin meet")
 		.call(d3.behavior.zoom().on("zoom", function () {
@@ -42,7 +42,7 @@ d3.json("One_Hot/ohe_step2_doc1_unigram.json", function(data) {
 		.style("stroke", "lightgrey")
 		.style("stroke-width", "1.5px");
 
-	var tooltip = d3.select("#ohe_step3")
+	var tooltip = d3.select("ohe_step3")
 		.append("div")
 		.attr('class', 'tooltip');
 
